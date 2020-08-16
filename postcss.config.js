bd07719e11300fcd26e53a7e1d7ac61bcd9e6081
Martin Ocando <martinocandop@gmail.com>
2020-08-16 17:45:28 -0400

    Initial Commit (config)

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    'postcss-import': {}, 
+    'postcss-preset-env': {
+      stage: 2,
+    },
+  },
+};
+	
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Tigrenator Beta</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main>
+        dsadsd
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
